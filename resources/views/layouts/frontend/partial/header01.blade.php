c0ad2544a6f7d630de4e4a8bb86608f421ee2ac4
MACEDO' Gerisvaldo <you@example.com>
2018-11-21 23:56:44 -0200

    first commit

---
+++ resources/views/layouts/frontend/partial/header01.blade.php
@@ -0,0 +1,75 @@
+
+<!-- start main menu -->
+<nav class="navbar main-menu">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar"
+                    aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div id="myNavbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown"><a href="index.html" class="dropdown-toggle " data-toggle="dropdown" role="button"
+                                        aria-haspopup="true" aria-expanded="false">home</a>
+                    <ul class="dropdown-menu">
+                        <li class="active"><a href="index.html">home one</a></li>
+                        <li><a href="home-2.html">home two</a></li>
+                        <li><a href="home-3.html">home three</a></li>
+                        <li><a href="home-4.html">home four</a></li>
+                        <li><a href="home-5.html">home five</a></li>
+                        <li><a href="home-6.html">home six</a></li>
+                        <li><a href="home-7.html">home seven</a></li>
+                        <li><a href="home-grid.html">home grid</a></li>
+                        <li><a href="list-home.html">home list</a></li>
+                        <li><a href="1st-large-grid.html">1st large then grid</a></li>
+                        <li><a href="1st-large-list.html">1st large then list</a></li>
+                        <li><a href="home-with-left-sidebar.html">home with left sidebar</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button"
+                                        aria-haspopup="true" aria-expanded="false">Gallery</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="gallery-with-2-columns.html">Gallery with 2 Columns</a></li>
+                        <li><a href="gallery-with-3-columns.html">Gallery with 3 Columns</a></li>
+                        <li><a href="gallery-with-4-columns.html">Gallery with 4 Columns</a></li>
+                        <li><a href="gallery-with-3-columns-mosaic.html">Gallery with Mosaic </a></li>
+                    </ul>
+                </li>
+                <li><a href="category-template.html" class="">travel</a></li>
+                <li class="dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button"
+                                        aria-haspopup="true" aria-expanded="false">Templates</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="about.html">about me</a></li>
+                        <li><a href="contact.html">contact one</a></li>
+                        <li><a href="contact-with-map.html">contact two</a></li>
+                        <li><a href="single-post.html">single page</a></li>
+                        <li><a href="category-template.html">travel</a></li>
+                    </ul>
+                </li>
+                <li><a href="about.html">About </a></li>
+                <li><a href="contact.html"> Contact</a></li>
+            </ul>
+            <div class="top-social-icons pull-right">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+                <a href="#"><i class="fa fa-pinterest"></i></a>
+                <a href="#"><i class="fa fa-heart"></i></a>
+                <a href="#"><i class="fa fa-google-plus"></i></a>
+
+                <div class="top-search">
+                    <a href="#" class="sactive"><i class="fa fa-search"></i></a>
+                </div>
+            </div>
+            <div class="show-search">
+                <form method="get" id="search-form" action="#">
+                    <input type="text" placeholder="Search and hit enter..." name="s" >
+                </form>
+            </div>
+        </div>
+    </div>
+</nav>
